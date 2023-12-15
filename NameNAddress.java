a8a97b87b1301cc162795d00c1b31c3f9d548fb5
whatthesquanch <146876837+whatthesquanch@users.noreply.github.com>
2023-12-15 12:34:18 -1000

    Add files via upload

---
+++ NameNAddress.java
@@ -0,0 +1,13 @@
+
+public class NameNAddress {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("First name: " + "Bibbity");
+		System.out.println("Last name: " + "Bob");
+		System.out.println("Street address: " + "123 Aloha Street");
+		System.out.println("City: " + "Mahalo");
+		System.out.println("Zip code: " + "80808");
+		
+	}
+}
